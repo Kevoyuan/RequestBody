2d924e15611bf628258e05de7dba0729e64e6d6e
Kevoyuan <kevinsyuan@gmail.com>
2023-05-13 22:51:09 +0200

    conf

--- match.py
+++ match.py
@@ -26,6 +26,87 @@ def match_model_in_text(text, dictionary):
     return matched_models   
 
 
+# def match_confs_in_text(text, dictionary):
+#     # Convert text to lowercase
+#     text = text.lower()
+
+#     # Sort keys from longest to shortest
+#     sorted_keys = sorted(dictionary.keys(), key=len, reverse=True)
+
+#     # Create a dictionary to hold matches
+#     matches = {}
+#     matched_confs = []
+    
+#     # Check if any key is in the text
+#     for key in sorted_keys:
+#         lower_key = key.lower()
+        
+#         lower_keys = lower_key.split()
+#         # Generate all permutations of the words
+#         lower_key_permutations = [' '.join(perm) for perm in permutations(lower_keys)]
+        
+
+#         # Check if any of these permutations are in the text
+#         if any(perm.lower() in text for perm in lower_key_permutations):
+        
+#             # matches[key] = dictionary[key]
+#             text = text.replace(lower_key, '')    
+
+#             matched_confs.append(dictionary[key])
+            
+#             # match_or = re.search(r'or|one of', text, re.IGNORECASE)    
+#             # if match_or:
+#             #     # replace only the first occurrence
+#             #     text = text.replace(match_or.group(), '',1)  
+#             #     matched_confs.append('/')
+        
+#     print(text)
+#     # return matches.values()
+#     return matched_confs
+
+# def match_confs_in_text(text, dictionary):
+#     # Convert text to lowercase
+#     text = text.lower()
+
+#     # Sort keys from longest to shortest
+#     sorted_keys = sorted(dictionary.keys(), key=len, reverse=True)
+
+#     # Create a list to hold matches
+#     matched_confs = []
+
+#     # Check if any key is in the text
+#     for key in sorted_keys:
+#         lower_key = key.lower()
+        
+#         lower_keys = lower_key.split()
+#         # Generate all permutations of the words
+#         lower_key_permutations = [' '.join(perm) for perm in permutations(lower_keys)]
+        
+#         # Check if any of these permutations are in the text
+#         for perm in lower_key_permutations:
+#             if perm.lower() in text:
+#                 text = text.replace(perm.lower(), '')
+#                 matched_confs.append(dictionary[key])
+#                 break  # Once a match is found, break the loop to avoid replacing other occurrences
+
+#     # Insert '/' between found key codes
+#     result = []
+#     Or = ['or', 'one of']
+#     And = ['and']
+#     AndNot = ['except']
+#     OrNot = ['or']
+#     for i in range(len(matched_confs)):
+#         result.append(matched_confs[i])
+#         if i != len(matched_confs) - 1 and any(op in text for op in Or):  # Only add '/' if any operator is in the remaining text
+#             result.append('/')
+#             for op in Or:
+#                 if op in text:
+#                     text = text.replace(op, '', 1)  # Remove one occurrence of operator from the text
+#                     break
+            
+#     print(text)
+#     return result
+
 def match_confs_in_text(text, dictionary):
     # Convert text to lowercase
     text = text.lower()
@@ -33,9 +114,11 @@ def match_confs_in_text(text, dictionary):
     # Sort keys from longest to shortest
     sorted_keys = sorted(dictionary.keys(), key=len, reverse=True)
 
-    # Create a dictionary to hold matches
-    matches = {}
+    # Create a list to hold matches
     matched_confs = []
+
+    # Define operators
+    # operators = ['or', 'one of']
 
     # Check if any key is in the text
     for key in sorted_keys:
@@ -46,14 +129,44 @@ def match_confs_in_text(text, dictionary):
         lower_key_permutations = [' '.join(perm) for perm in permutations(lower_keys)]
         
         # Check if any of these permutations are in the text
-        if any(perm.lower() in text for perm in lower_key_permutations):
-        
-            # matches[key] = dictionary[key]
-            text = text.replace(lower_key, '')
-            matched_confs.append(dictionary[key])
-    # return matches.values()
-    return matched_confs
+        for perm in lower_key_permutations:
+            if perm.lower() in text:
+                text = text.replace(perm.lower(), '')
+                matched_confs.append(dictionary[key])
+                break  # Once a match is found, break the loop to avoid replacing other occurrences
 
+    result = []
+    
+    # Insert '/-' between found key codes if 'or ... without' pattern is present
+    OrNot_pattern = r'\bor .*? without\b'
+    OrNot_match = re.search(OrNot_pattern, text)
+    OrNot_operator = '/-'
+    
+    Or_pattern = r'\bor\b'
+    Or_match = re.search(Or_pattern, text)
+    Or_operator = '/'
+    
+    Not_pattern = r'\bwithout\b'
+    Not_match = re.search(Not_pattern, text)
+    Not_operator = '-'
+    
+    And_operator = '+'
+    
+    if OrNot_match:
+        
+        result.append('+' + OrNot_operator.join(matched_confs))
+    elif Or_match:
+        
+        result.append('+' + Or_operator.join(matched_confs))
+    elif Not_match:
+        
+        result.append('+' + Not_operator.join(matched_confs))
+    else:
+        
+        result.append('+' + And_operator.join(matched_confs))    
+
+
+    return result
 
 def date_in_text(text):
     # Clean the text
@@ -100,3 +213,50 @@ def date_in_text(text):
         
     return formatted_date
 
+
+text = "I am planning to order the IX with a sunroof or panorama glass roof sky lounge, and the EU Comfort Package or M Sport Package on 12th April 2018. Is this configuration possible?"
+
+SteeringWheel_mapping = {
+    'Left-Hand': 'LL',
+    'Right-Hand': 'RL',
+}
+
+AvailablePackages_mapping = {
+    'M Sport': 'P337A',
+    'M Sport Pro': 'P33BA',
+    'Comfort EU': 'P7LGA',
+}
+
+Roof_mapping = {
+    'Panorama Glass Roof': 'S402A',
+    'Panorama Glass Roof Sky Lounge': 'S407A',
+    'Sunroof': 'S403A',
+}
+
+Model_mapping = {
+    'iX xDrive50': '21CF',
+    'iX xDrive40': '11CF',
+    'X7 xDrive40i': '21EM',
+    'X7 xDrive40d': '21EN',
+    'M8': 'DZ01',
+    '318i': '28FF'
+}
+
+# Model Type
+modelTypeCodes = match_model_in_text(text, Model_mapping)
+print(modelTypeCodes)
+
+# Configuration
+Roof_matches_code = match_confs_in_text(text, Roof_mapping)
+AvailablePackages_matches_code = match_confs_in_text(text, AvailablePackages_mapping)
+SteeringWheel_matches_code = match_confs_in_text(text, SteeringWheel_mapping)
+# conf_list = Roof_matches_code + AvailablePackages_matches_code + SteeringWheel_matches_code
+
+print(f'Roof_matches_code: {Roof_matches_code}')  
+print(f'AvailablePackages_matches_code: {AvailablePackages_matches_code}')  
+print(SteeringWheel_matches_code)  
+# print(conf_list)
+
+# Dates
+date = date_in_text(text)
+print(date)  
